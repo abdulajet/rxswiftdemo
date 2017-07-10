86b70e042e6db8b3a547a7cfd7c570c6bf3f3a33
Abdul Ajetunmobi <abdul.ajetunmobi@starlingbank.com>
2017-07-10 16:22:35 +0100

    Initial Commit

---
+++ cookiesnapkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cookiesnapkit
+//
+//  Created by Abdul Ajetunmobi on 10/07/2017.
+//  Copyright © 2017 Abdul Ajetunmobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
