c0f9dafaf1ebb49916b9a2673f35051b2ff9298c
Abdul Ajetunmobi <abdul.ajetunmobi@starlingbank.com>
2017-07-11 15:00:52 +0100

    Added image and cleaned up

--- cookiesnapkit/ViewController.swift
+++ cookiesnapkit/ViewController.swift
@@ -12,45 +12,56 @@ import RxSwift
 
 class ViewController: UIViewController {
 	
-	let cookie = UIButton(type: UIButtonType.system)
+	//init variables
+	let cookie = UIButton()
 	let label = UILabel()
+	
+	//counter as a RxVariable
 	var counter = Variable(0)
 	let disposeBag = DisposeBag()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		//store superview for convenience
 		let superview = self.view!
 		
-		view.backgroundColor = UIColor.gray
+		superview.backgroundColor = UIColor.gray
 		
-		cookie.backgroundColor = UIColor.brown
-		cookie.setTitle(".", for: .normal)
+		//set cookie image onto the button
+		cookie.setImage(#imageLiteral(resourceName: "cookie"), for: .normal)
 		superview.addSubview(cookie)
 		
+		//snapkit constraints
 		cookie.snp.makeConstraints { (make) in
-			make.width.equalTo(50)
-			make.height.equalTo(50)
+			make.width.equalTo(100)
+			make.height.equalTo(100)
 			make.center.equalTo(superview)
 		}
 		
+		//add func as target for touches on the button
 		cookie.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
 		
-		label.textColor = UIColor.red
-		label.text = "0"
+		//set up the label
+		label.font = label.font.withSize(20)
 		superview.addSubview(label)
 		
+		//label contraints
 		label.snp.makeConstraints { (make) in
-			make.bottom.equalTo(superview.snp.bottom).offset(-50)
+			make.bottom.equalTo(superview.snp.bottom).offset(-200)
 			make.centerX.equalTo(superview.snp.centerX)
 		}
 		
+		//subscribe to the button
 		counter.asObservable().subscribe{event in
+			//on next update the label
 			self.label.text = String(self.counter.value)
 		}.addDisposableTo(disposeBag)
 		
 	}
 	
 	func buttonClicked(){
+		//increment counter
 		counter.value = counter.value + 1
 	}
 }
